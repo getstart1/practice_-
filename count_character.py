d788393ec284e88c914cc8065a19c8623dcd3023
qdai <qdai0723@gmail.com>
2019-01-04 14:36:51 +0800

    count how many characters, add link to the problem

--- count_character.py
+++ count_character.py
@@ -1,3 +1,5 @@
+#计算字符个数
+#https://www.nowcoder.com/practice/a35ce98431874e3a820dbe4b2d0508b1?tpId=37&tqId=21225&tPage=1&rp=&ru=%2Fta%2Fhuawei&qru=%2Fta%2Fhuawei%2Fquestion-ranking
 import sys
 strList = []
 count = 0
