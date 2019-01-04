28495edba43d867e0c1f9b5a04effbabaf4c2851
qdai <qdai0723@gmail.com>
2019-01-04 14:38:15 +0800

    add the link for problem one

--- count_last.py
+++ count_last.py
@@ -1,3 +1,5 @@
+#Count the length of the last word in a string
+#https://www.nowcoder.com/practice/8c949ea5f36f422594b306a2300315da?tpId=37&tqId=21224&tPage=1&rp=&ru=/ta/huawei&qru=/ta/huawei/question-ranking
 string = input()
 for i in range(len(string)):
     if (string[len(string)- i -1] != ' '):
